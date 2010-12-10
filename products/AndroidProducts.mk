c2f479f069ec67e0bf816293e053ad27d4793b54
Robert Burns <burnsra@gmail.com>
2010-12-09 22:43:14 -0500

    Misc cleanup
    Change-Id: I0be473442b2cd111114bc00c122ab8d7f2d149b1

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,11 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/mau5_passion.mk \
-    $(LOCAL_DIR)/mau5_vision.mk \
-    $(LOCAL_DIR)/mau5_sholes.mk \
-    $(LOCAL_DIR)/mau5_supersonic.mk \
-    $(LOCAL_DIR)/mau5_bravoc.mk \
-    $(LOCAL_DIR)/mau5_inc.mk \
-    $(LOCAL_DIR)/mau5_espresso.mk \
-    $(LOCAL_DIR)/mau5_liberty.mk \
-    $(LOCAL_DIR)/mau5_legend.mk \
-    $(LOCAL_DIR)/mau5_dream_sapphire.mk
+    $(LOCAL_DIR)/gingerbread_green_inc.mk
