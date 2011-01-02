65db2d7a127581385c5a3ea21ff7ba31df636c38
Robert Burns <burnsra@gmail.com>
2011-01-02 18:45:39 -0500

    Removed unneeded resources

--- products/gingerbread_inc.mk
+++ products/gingerbread_inc.mk
@@ -6,10 +6,3 @@ PRODUCT_NAME := gingerbread_inc
 ## common stuff ##
 PRODUCT_PACKAGE_OVERLAYS := vendor/burnsra/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
-
-PRODUCT_COPY_FILES += \
-    vendor/burnsra/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh \
-    vendor/burnsra/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
-    vendor/burnsra/prebuilt/common/app/LatinIME.apk:system/app/LatinIME.apk
-
-PRODUCT_PACKAGES += ThemeDefaults
