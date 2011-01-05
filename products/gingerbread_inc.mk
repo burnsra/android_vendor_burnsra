534df9a655d0c62e0af6695eebff9351c039b04c
Robert Burns <burnsra@gmail.com>
2011-01-05 11:09:50 -0500

    Added additional packages

--- products/gingerbread_inc.mk
+++ products/gingerbread_inc.mk
@@ -3,6 +3,11 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_inc.mk)
 
 PRODUCT_NAME := gingerbread_inc
 
+# Include the FM Radio
+PRODUCT_PACKAGES += FM
+# Include the Flashlight/Torch
+PRODUCT_PACKAGES += Torch
+
 ## common stuff ##
 PRODUCT_PACKAGE_OVERLAYS := vendor/burnsra/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
