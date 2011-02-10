1211865e970f52e11b3a2963a938299f54d841df
Robert Burns <burnsra@gmail.com>
2011-02-09 20:46:21 -0500

    Added Pacman overlay

--- products/gingerbread_inc.mk
+++ products/gingerbread_inc.mk
@@ -3,10 +3,6 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_inc.mk)
 
 PRODUCT_NAME := gingerbread_inc
 
-# Include the FM Radio
-PRODUCT_PACKAGES += FM
-# Include the Flashlight/Torch
-PRODUCT_PACKAGES += Torch
 # Build Gingerbread theme
 PRODUCT_PACKAGES += Gingerbread
 
@@ -18,8 +14,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/common
 # include vendor/burnsra/products/themes.mk
 
 # Build kernel
-PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
 PRODUCT_SPECIFIC_DEFINES += TARGET_BUILD_KERNEL=true
+PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
 PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=htc-kernel-incrediblec
 PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_incrediblec_defconfig
 
